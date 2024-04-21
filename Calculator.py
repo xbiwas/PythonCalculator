75b4af15b38998e5659b10f20ded249771bdccf7
xbiwas <biwas2059@gmail.com>
2024-04-21 17:29:57 +0545

    Added functions to add, sub, multiply and divide

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,13 @@
+def add(x,y):
+    return x + y
+def sub(x,y):
+    return x - y
+def multiply(x,y):
+    return x * y
+def divide(x,y):
+    return x/y
+
+
 print("Welcome to do some of the calculations")
 print("Select first number")
 num1 = int(input())
@@ -6,19 +16,19 @@ num2 = int(input())
 
 print("Now what do you wanna do with those two numbers")
 print("DO you wanna add it, subtract it, multiply it or divide it")
-print("Please select an operator from the list below")
+print("Please select an operator from the list below:")
 operators = ["+", "-", "*", "/"]
 print("The operators are: ", operators)
 selectOperator = input()
 
 if selectOperator == operators[0]:
-    result = num1 + num2
+    result = add(num1,num2)
     print(result)
 elif selectOperator == operators[1] :
-    result = num1 - num2
+    result = sub(num1,num2)
     print(result)
 elif selectOperator == operators[2] :
-    result = num1 * num2
+    result = multiply(num1, num2)
     print(result)
 else:
     print("You have selected an invalid operator")
